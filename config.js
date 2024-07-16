cfa452a7a551b39e4cc1a761b41eea39ffeda8e2
BigIrishLion <8316984+bigirishlion@users.noreply.github.com>
2024-07-15 20:50:00 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Spectral', // <title>
-  manifestName: 'Spectral',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'Mason Built Contruction', // <title>
+  manifestName: 'Mason Built Contruction',
+  manifestShortName: 'Mason Built Contruction', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  pathPrefix: `/about/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Mason Built Contruction',
+  subHeading: 'Contruction Done Right. The First Time',
   // social
   socialLinks: [
     {
       style: 'brands',
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com',
     },
     {
       style: 'brands',
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com',
     },
     {
       style: 'solid',
